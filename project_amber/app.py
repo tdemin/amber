be69464d1c493ef61089bfc816b1b9ef09e4cbde
Timur Demin <me@tdem.in>
2019-06-03 00:23:08 +0500

    Add a basic config loader

--- project_amber/app.py
+++ project_amber/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from project_amber.config import config
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = config["database"]
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
---
+++ project_amber/config.py
@@ -0,0 +1,36 @@
+import os
+from json import load
+
+if os.name == "posix":
+    configPaths = ["./config.json", "/etc/amber.json"]
+else:
+    configPaths = ["config.json"]
+
+config = {
+    "database": ""
+}
+
+# search for every file name and load the config from the first file
+# that exists
+for testedFileName in configPaths:
+    if os.path.isfile(testedFileName):
+        # holds the actual config file name
+        configFileName = testedFileName
+        break
+if not "configFileName" in globals():
+    print("No configuration file found, exiting")
+    exit(1)
+
+try:
+    with open(configFileName, encoding="utf8") as configFile:
+        # holds the new configuration variables which replace the
+        # default ones
+        loadedConfig = load(configFile)
+except OSError as ioerr:
+    print("Could not open config file", configFileName)
+    print(ioerr.strerror)
+    exit(1)
+
+for entry in config:
+    if entry in loadedConfig:
+        config[entry] = loadedConfig[entry]
