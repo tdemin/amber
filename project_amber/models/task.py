55ae1b446c2396357a7c832d14ad90c1d027d46b
Timur Demin <me@tdem.in>
2019-06-08 00:55:01 +0500

    Add the task model

---
+++ project_amber/models/task.py
@@ -0,0 +1,15 @@
+from project_amber.db import db
+
+class Task(db.Model):
+    """
+    Task model. Contains a task ID, the owner, the subject, and the lastmod /
+    creation time.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    text = db.Column(db.String(65536)) # TODO: probably subject to increase
+    creation_time = db.Column(db.Integer, nullable=False)
+    last_mod_time = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return "<Task id='%d' owner='%d' text='%s' creation_time='%d'>" \
+            % self.id, self.owner, self.text, self.creation_time
