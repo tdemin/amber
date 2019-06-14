526d2ea3b45a12df2d9ad4182f36e0a02262242b
Timur Demin <me@tdem.in>
2019-06-14 11:13:07 +0500

    Change the home page URI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="project_amber",
     version="0.0.1",
     description="The backend app of a note-taking app, Project Amber",
-    url="https://git.tdem.in/amber/backend",
+    url="https://git.tdem.in/tdemin/amber",
     author="Timur Demin",
     author_email="me@tdem.in",
     license="MIT",
@@ -18,7 +18,9 @@ setup(
         "Programming Language :: Python :: 3"
     ],
     keywords="tasks backend flask",
-    # project_urls={}
+    project_urls={
+        "Homepage": "https://git.tdem.in/tdemin/amber"
+    },
     packages=["project_amber"],
     install_requires=[
         "flask",
