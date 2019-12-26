9e2b7f69562507c208512b3997b232fa495eb0d9
Timur Demin <me@tdem.in>
2019-12-26 18:21:10 +0500

    Expose information on whether signup is allowed on the server

--- project_amber/handlers/misc.py
+++ project_amber/handlers/misc.py
@@ -1,8 +1,12 @@
 from json import dumps
 
+from project_amber.config import config
 from project_amber.const import VERSION
-from project_amber.handlers.const import API_VERSION
+from project_amber.handlers.const import API_VERSION, API_SIGNUP
+
+signup_allowed = False
+if config["allow_signup"]: signup_allowed = True
 
 
 def version():
-    return dumps({API_VERSION: VERSION})
+    return dumps({API_VERSION: VERSION, API_SIGNUP: signup_allowed})
