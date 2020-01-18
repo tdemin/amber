e43ac1f301334300c998fa4870ba51bcfaebcfff
Timur Demin <me@tdem.in>
2020-01-19 00:50:18 +0500

    Bump v0.0.4

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ from setuptools import setup
 
 setup(
     name="project_amber",
-    version="0.0.3",
+    version="0.0.4",
     description="The backend app of a note-taking app, Project Amber",
     url="https://git.tdem.in/tdemin/amber",
     author="Timur Demin",
     author_email="me@tdem.in",
     license="MIT",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha", "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6", "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
         "Programming Language :: Python :: 3"
     ],
